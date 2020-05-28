549f9505252b2f889f4036a348bcb9858f3a01a1
Kino Kiaro <kiarokino@gmail.com>
2020-05-28 18:35:14 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :articles
+  has_many :comments
+  has_many :article_likes
 end
