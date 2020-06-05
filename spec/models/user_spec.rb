92eb8f3fbdd778d7e10346f03da92e64f4c1aad1
Kino Kiaro <kiarokino@gmail.com>
2020-06-05 11:37:22 +0900

    Userモデル–emailカラムのバリデーションテストを記述（入力値は固定）。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "emailを指定しているとき" do
+    it "ユーザーがつくられる" do
+      user = User.new(name:"ひろあき", password:"hiroakinopass" ,email:"narizawa@testmail.com")
+      expect(user).to be_valid
+    end
+  end
+
+  context "emailがブランクのとき" do
+    it "エラーする" do
+      user = User.new(name:"ひろあき", password:"hiroakinopass" ,email:nil)
+      user.valid?
+      expect(user.errors.messages[:email]).to include "is invalid"
+    end
+  end
+
+  context "同じemailが存在するとき" do
+    it "エラーする" do
+      User.create!(name:"ひろあき", password:"hiroakinopass" , email:"narizawa@testmail.com")
+      user = User.new(name:"ひろあき", password:"hiroakinopass" , email:"narizawa@testmail.com")
+      user.valid?
+      expect(user.errors.messages[:email]).to include "has already been taken"
+    end
+  end
+
+
 end
