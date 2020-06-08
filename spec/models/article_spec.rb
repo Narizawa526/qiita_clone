65a801c23855fcc1a3ae9f9222d679c3a9b263b7
Kino Kiaro <kiarokino@gmail.com>
2020-06-08 22:45:55 +0900

    spec/models/article_spec.rb にバリデーションに引っかかる場合のテストを実装。

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
+
   context "title、bodyが指定されているとき" do
     let(:article){build(:article)}
     it "投稿される" do 
       expect(article).to be_valid
     end
   end
+
+  context "titleが空のとき" do
+    let(:article){build(:article, title: nil)}
+    it "投稿されない" do 
+      article.valid?
+      expect(article.errors.messages[:title]).to include "can't be blank"
+    end
+  end
+
+  context "bodyが空のとき" do
+    let(:article){build(:article, body: nil)}
+    it "投稿されない" do 
+      article.valid?
+      binding.pry
+      expect(article.errors.messages[:body]).to include "can't be blank"
+    end
+  end
+
 end
